934c98f687757f33c0fe225163e634cf5f224a8b
Yaroslava Mala <ymala@kse.org.ua>
2023-09-29 14:21:59 +0300

    added separate file UsersLoader.cs

--- LastSeenTask/Program.cs
+++ LastSeenTask/Program.cs
@@ -1,45 +1,28 @@
-﻿using Newtonsoft.Json;
-using LastSeenTask;
+﻿using LastSeenTask;
 
-var offset = 0;
 using (HttpClient client = new HttpClient())
 {
+    var usersLoader = new UsersLoader(client);
+    var offset = 0;
+
     while (true)
     {
-        var apiUrl = $"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}";
-        var response = client.GetAsync(apiUrl).Result;
-        if (response.IsSuccessStatusCode)
-        {
-            var json =  response.Content.ReadAsStringAsync().Result;
-            var userData = JsonConvert.DeserializeObject<UserData>(json);
-            var formatter = new LastSeenFormatter();
-            if (userData.data == null || userData.data.Length == 0)
-            {
-                return;
-            }
-            
-            foreach (User user in userData.data)
-            {
-                var formattedLastSeen = formatter.Format(DateTimeOffset.Now, user.LastSeenDate.GetValueOrDefault());
-                Console.WriteLine($"Nickname: {user.Nickname}");
-                Console.WriteLine($"Last Seen: {formattedLastSeen}");
-                Console.WriteLine($"Is Online: {user.IsOnline}\n");
-            }
-            offset += 5;
-        }
-        else
+        var userData = usersLoader.LoadUsers(offset);
+
+        if (userData.data == null || userData.data.Length == 0)
         {
             break;
         }
+
+        var formatter = new LastSeenFormatter();
+        foreach (User user in userData.data)
+        {
+            var formattedLastSeen = formatter.Format(DateTimeOffset.Now, user.LastSeenDate.GetValueOrDefault());
+            Console.WriteLine($"Nickname: {user.Nickname}");
+            Console.WriteLine($"Last Seen: {formattedLastSeen}");
+            Console.WriteLine($"Is Online: {user.IsOnline}\n");
+        }
+
+        offset += 5;
     }
-}
-public class UserData
-{
-    public User[] data { get; set; }
-}
-public class User
-{
-    public DateTimeOffset? LastSeenDate { get; set; }
-    public string Nickname { get; set; }
-    public bool IsOnline { get; set; }
 }
---
+++ LastSeenTask/UsersLoader.cs
@@ -0,0 +1,43 @@
+using Newtonsoft.Json;
+using LastSeenTask;
+
+public class UserData
+{
+    public User[] data { get; set; }
+}
+
+public class User
+{
+    public DateTimeOffset? LastSeenDate { get; set; }
+    public string Nickname { get; set; }
+    public bool IsOnline { get; set; }
+}
+
+public class UsersLoader
+{
+    private readonly HttpClient _client;
+    private readonly LastSeenFormatter _formatter;
+
+    public UsersLoader(HttpClient httpClient)
+    {
+        _client = httpClient;
+        _formatter = new LastSeenFormatter();
+    }
+
+    public UserData LoadUsers(int offset)
+    {
+        var apiUrl = $"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}";
+        var response = _client.GetAsync(apiUrl).Result;
+
+        if (response.IsSuccessStatusCode)
+        {
+            var json = response.Content.ReadAsStringAsync().Result;
+            var userData = JsonConvert.DeserializeObject<UserData>(json);
+            return userData;
+        }
+        else
+        {
+            return new UserData();
+        }
+    }
+}

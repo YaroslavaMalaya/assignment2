9df5a81616f0c4957a514838f0ea462613d7661f
Yaroslava Mala <ymala@kse.org.ua>
2023-09-29 10:55:43 +0300

    added receipt of data in the API response

--- LastSeenTask/Program.cs
+++ LastSeenTask/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Newtonsoft.Json;
 
-Console.WriteLine("Hello, World!");
+var offset = 0;
+using (HttpClient client = new HttpClient())
+{
+    while (true)
+    {
+        var apiUrl = $"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}";
+        var response = client.GetAsync(apiUrl).Result;
+
+        if (response.IsSuccessStatusCode)
+        {
+            var json =  response.Content.ReadAsStringAsync().Result;
+            var userData = JsonConvert.DeserializeObject<UserData>(json);
+            offset += 5;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+public class UserData
+{
+    public User[] data { get; set; }
+}
+
+public class User
+{
+    public DateTimeOffset? LastSeenDate { get; set; }
+    public string Nickname { get; set; }
+    public bool IsOnline { get; set; }
+}
